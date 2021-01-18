649ac2a248505ad597520dd5eeae04416d626f29
Lilybg <lilybgoldman@gmail.com>
2021-01-18 10:09:53 -0800

    added test cases

--- perm_lex_testcases.py
+++ perm_lex_testcases.py
@@ -5,6 +5,9 @@ class TestAssign1(unittest.TestCase):
 
     def test_perm_gen_lex(self):
         self.assertEqual(perm_lex.perm_gen_lex('ab'),['ab','ba'])
+        self.assertEqual(perm_lex.perm_gen_lex('abc'),['abc','acb','bac','bca','cab','cba'])
+        self.assertEqual(perm_lex.perm_gen_lex(''),[])
+        self.assertEqual(perm_lex.perm_gen_lex('b'),['b'])
 
 if __name__ == "__main__":
         unittest.main()

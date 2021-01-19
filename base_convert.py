b1208342ec22d4a77d248352b4075b29f4e3e50c
Lilybg <lilybgoldman@gmail.com>
2021-01-19 15:19:05 -0800

    complete

--- base_convert.py
+++ base_convert.py
@@ -1,15 +1,52 @@
 #int => str
+#
 def convert(num, b):
-    """Recursive function that returns a string 
-    representing num in the base b"""
     if int(num/b) == 0:
-        return str(num%b)
+        if b < 10:
+            return str(num%b)
+        else:
+            if num%b == 10:
+                return 'A'
+            elif num%b == 11:
+                return 'B'
+            elif num%b == 12:
+                return 'C'
+            elif num%b == 13:
+                return 'D'
+            elif num%b == 14:
+                return 'E'
+            elif num%b == 15:
+                return 'F'
+            else: 
+                return str(num%b)
 
-    reduced = num//b
-    y = convert(reduced,b)
+
+
+    if b < 10:
+        reduced = num//b
+        y = convert(reduced,b)
+        return y + str(num%b)
+
+    else:
+        reduced = num//b
+        remainder = num%b
+        y = convert(reduced,b)
+
+        if remainder == 10:
+            return y + 'A'
+        elif remainder == 11:
+            return y + 'B'
+        elif remainder == 12:
+            return y + 'C'
+        elif remainder == 13:
+            return y + 'D'
+        elif remainder == 14:
+            return y + 'E'
+        elif remainder == 15:
+            return y + 'F'
+        else:
+            return y + str(num%b)
+
     
-    return y + str(num%b)
 
-    
-
-print(convert(30,4))
+print(convert(8001,12))

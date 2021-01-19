3eb26c79066b7829946c56c382ee6fbe60d6d033
Lilybg <lilybgoldman@gmail.com>
2021-01-18 23:48:01 -0800

    start

--- base_convert.py
+++ base_convert.py
@@ -1,3 +1,13 @@
-
+#int => str
 def convert(num, b):
-    """Recursive function that returns a string representing num in the base b"""
+    """Recursive function that returns a string 
+    representing num in the base b"""
+    if num < b:
+        return num
+
+    reduced = num/b
+    dig = str(num%b)
+
+    return dig + convert(reduced,b)
+
+print(convert(20,2))

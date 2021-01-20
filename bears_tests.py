1d910d51e0eac5a6837e300fef32de314cb6e3f8
Lilybg <lilybgoldman@gmail.com>
2021-01-19 22:07:13 -0800

    added test cases

--- bears_tests.py
+++ bears_tests.py
@@ -13,6 +13,9 @@ class TestAssign1(unittest.TestCase):
 
     def test_bear_04(self):
         self.assertFalse(bears(41))
+    
+    def test_bear_05(self):
+        self.assertTrue(bears(210))
 
 if __name__ == "__main__":
     unittest.main()

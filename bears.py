7ae5ab4a483192c7b3be7a6b38eabf7a1cf82aae
Lilybg <lilybgoldman@gmail.com>
2021-01-20 16:32:44 -0800

    comments

--- bears.py
+++ bears.py
@@ -10,36 +10,30 @@ def bears(n):
 
     if n/2 == n//2:
         if n//2 > 42:
-            t = 1
-            q = 1
-            r = 0
-            s = 0
+            t = q = 1   #allows it to run the divide by 2 case only once
+            r = 3 = 0
         else:
-            t = 0
-            q = 0
-            r = 0
-            s = 0
+            t = q = r = s = 0
     else:
-        t = 0
-        q = 0
-        r = 0
-        s = 0
+        t = q = r = s = 0
+
     if n/3 == n//3 or n/4 == n//4:
         lval = n%10
-        lval_2 = (n%100 - n%10)//10
+        lval_2 = (n%100 - n%10)//10 
         lval_3 = lval * lval_2
-        if lval_3 != 0:
-            r = 1
+        if lval_3 != 0:         #stops it from allowing it to run that case if it will only add 0 to itself
+            r = 1               #allows it to run the subtract by last 2 digits multiplied case only once
             t = t + 1
         else:
             r=0
     else:
         r=0
+    
     if n/5 == n//5:
-        t = t + 1
-        s = 1
+        t = t + 1            
+        s = 1           #allows it to run the subtract by 42 case only once
 
-    if r == q == s == 0:
+    if r == q == s == 0:    #fails if it can't do any of the cases
         return False
 
     while t != 0:
